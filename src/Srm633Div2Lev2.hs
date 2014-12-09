4507bb8eb9faef61814b48d023f0ec4ab54fd767
snowyunee <snowyunee@gmail.com>
2014-12-09 10:29:43 +0900

    Create Srm633Div2Lev2.hs

---
+++ src/Srm633Div2Lev2.hs
@@ -0,0 +1,23 @@
+-- | Main entry point to the application.
+module Main where
+ 
+import Data.List   
+ 
+able_to_get x y len =
+    let
+        sum = Data.List.sum len
+        max = maximum len
+        distance = sqrt (x*x + y*y)
+    in
+        if sum >= distance &&
+           max <= sum - max + distance then "Able"
+        else
+           "Not able"
+   
+-- | The main entry point.
+main :: IO ()
+main = do
+    print $ able_to_get 5 4 [2, 5]
+    print $ able_to_get 3 4 [4]
+    print $ able_to_get 3 4 [6]
+    print $ able_to_get 0 1 [100, 100]
