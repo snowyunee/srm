fb8e7b8a0d0362eb67dd8b966b9aff142db627c5
unknown <snowyunee@gms-snowyunee.ds.neowiz.com>
2014-12-09 17:06:22 +0900

    632 cpp version

---
+++ src/Srm632Div2Lev1.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+
+int numberOfLap(int N, std::vector<int> d)
+{
+  if (d.empty()) return 0;
+
+  std::vector<int> count(d.size(), 0);
+  std::transform(d.begin() + 1, d.end(),
+    d.begin(), count.begin(),
+    [](const int& x2, const int& x1) { return (x1 >= x2) ? 1 : 0; });
+
+  return std::accumulate(count.begin(), count.end(), 0) + 1;
+
+}
+
+int main()
+{
+  auto print = [](auto x) { std::cout << x << std::endl; };
+
+  print(numberOfLap(3, { 1, 2, 3 }));
+  print(numberOfLap(24, { 6, 6 }));
+  print(numberOfLap(3, { 3, 2, 1 }));
+  print(numberOfLap(50, { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 }));
+
+}
+
---
+++ src/Srm632Div2Lev2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+int numberOfSubsequences(const std::vector<int>& d)
+{
+  auto test = [](auto i, auto j) -> bool
+  {
+    if (i == j) return true;
+    if (i + 1 == j) return true;
+
+    auto diff = std::vector<int>(j - i);
+    std::transform(i, j, i + 1, diff.begin(),
+      [](auto k1, auto k2) { return k2 - k1; });
+    return std::all_of(diff.begin(), diff.end(),
+      [&](int x) { return (x == diff[0]) ? true : false; });
+  };
+
+  auto count = 0;
+  for (auto i = d.begin(); i != d.end(); i++)
+  {
+    for (auto j = i; j != d.end(); j++)
+    {
+      if (test(i, j))
+        count++;
+    }
+  }
+  return count;
+}
+
+int main()
+{
+  auto print = [](auto x) { std::cout << x << std::endl; };
+
+  print(numberOfSubsequences({ 0, 1, 2 }));
+  print(numberOfSubsequences({ 1, 2, 4 }));
+  print(numberOfSubsequences({ 3, 2, 1, 0 }));
+  print(numberOfSubsequences({ 1, 2, 4, 8, 16 }));
+  print(numberOfSubsequences({ 1, 3, 5, 5, 5, 5, 64, 4, 23, 2, 3, 4, 5, 4, 3 }));
+}
