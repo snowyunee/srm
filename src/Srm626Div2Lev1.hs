94c396e97073d2f2573c4931ff8099ff4d3079b4
snowyunee <snowyunee@neowiz.com>
2015-01-27 11:20:21 +0900

    0.1 Srm626Div2Lev1

---
+++ src/Srm626Div2Lev1.hs
@@ -0,0 +1,15 @@
+
+--getExceptions :: Integral a => a -> a -> Double
+getExceptions a b  =
+    --(\ls -> (fromIntegral (sum ls)) / (fromIntegral (length ls))) $
+    fromIntegral.length $
+    [x| x <- [1,2..a], y <- [1,2..b], x > y]
+   
+
+-- | The main entry point.
+main :: IO ()
+main = do
+    print $ getExceptions 2 2 -- 2.0
+    print $ getExceptions 4 2 -- 3.2
+    print $ getExceptions 3 3 -- 2.66666..
+    print $ getExceptions 11 13 -- 7.99999..
