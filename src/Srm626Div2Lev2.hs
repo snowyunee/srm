721f1557e5757cfea480304e2297efbc42ccdf29
snow <snowyunee@gmail.com>
2015-02-03 01:55:35 -0800

    resolve stack overflow problem

--- src/Srm626Div2Lev2.hs
+++ src/Srm626Div2Lev2.hs
@@ -1,7 +1,13 @@
+import Data.List
+
+sum' :: Integral a => [a] -> a
+sum' xs = foldl' (\acc x -> acc `seq` x `seq` acc + x) 0 xs
+
+length' xs = foldl' (\acc x -> acc `seq`  acc + 1) 0 xs
 
 getExceptions :: (Integral a, Fractional b) => a -> a -> b
 getExceptions a b  =
-    (\ls -> fromIntegral (sum ls) / fromIntegral (length ls)) $
+    (\ls -> fromIntegral (sum' ls) / fromIntegral (length ls)) $
     [x| x <- [1..a], y <- [1..b], x > y]
    
 
@@ -12,3 +18,4 @@ main = do
     print $ getExceptions 4 2 -- 3.2
     print $ getExceptions 3 3 -- 2.66666..
     print $ getExceptions 11 13 -- 7.99999..
+    print $ getExceptions 2000 2000 
